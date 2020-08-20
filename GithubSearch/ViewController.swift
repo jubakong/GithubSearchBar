9ca9d15eded716ee03d02e87b0039978b03ec273
jubakong <jubakong@gmail.com>
2020-08-20 13:53:24 +0900

    Initial Commit

---
+++ GithubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by Jeongbae Kong on 2020/08/20.
+//  Copyright © 2020 Jeongbae Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
