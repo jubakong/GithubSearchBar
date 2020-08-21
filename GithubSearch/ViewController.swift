bcb2cf961bc5959d2fd85004431fd43ddaad86cd
jubakong <jubakong@gmail.com>
2020-08-21 14:13:27 +0900

    Added refresh

--- GithubSearch/ViewController.swift
+++ GithubSearch/ViewController.swift
@@ -13,24 +13,23 @@ import RxCocoa
 class ViewController: UIViewController {
   
   @IBOutlet weak var searchBar: UISearchBar!
-  
-  
   @IBOutlet weak var tableView: UITableView!
   
   let viewModel = ViewModel()
-  
   let disposeBag = DisposeBag()
+  private lazy var refreshControl = UIRefreshControl()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     setup()
+    addRefreshControlCollectionView()
   }
   
   private func setup() {
     
     let input = ViewModel.Input(
       searchBar: searchBar.rx.text.orEmpty.asDriver()
-      )
+    )
     
     let output = viewModel.transform(input: input)
     
@@ -40,7 +39,34 @@ class ViewController: UIViewController {
         cell.textLabel?.text = repository.repoName
         cell.detailTextLabel?.text = repository.repoURL
     }
-  .disposed(by: disposeBag)
+    .disposed(by: disposeBag)
+  }
+  
+  private func addRefreshControlCollectionView() {
+    let colorOption = [NSAttributedString.Key.foregroundColor : UIColor.white]
+    
+    let title = NSAttributedString(string: "pull to refresh", attributes: colorOption)
+    
+    refreshControl.attributedTitle = title
+    refreshControl.backgroundColor = .blue
+    refreshControl.tintColor = .white
+    refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+    
+    if #available(iOS 10.0, *) {
+      tableView.refreshControl = refreshControl
+    } else {
+      tableView.addSubview(refreshControl)
+    }
+  }
+  
+  @objc private func refresh() {
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {[weak self] in
+      guard let `self` = self else { return }
+      
+      self.tableView.refreshControl?.endRefreshing()
+      self.tableView.reloadData()
+    }
   }
 }
 
